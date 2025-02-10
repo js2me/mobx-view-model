5f69a2fdae1f0dcc813a0b51e80104d56751bdd7
Sergey Volkov <js2me@outlook.com>
2025-02-10 08:13:46 +0300

    test: add more tests for useCreateViewModel

--- src/hooks/use-create-view-model.test.tsx
+++ src/hooks/use-create-view-model.test.tsx
@@ -6,6 +6,7 @@ import {
   ViewModelStore,
   ViewModelsProvider,
   useCreateViewModel,
+  useViewModel,
 } from '../index.js';
 import { ViewModelBaseMock } from '../view-model/view-model.base.test.js';
 import { ViewModelStoreBaseMock } from '../view-model/view-model.store.base.test.js';
@@ -39,6 +40,54 @@ describe('useCreateViewModel', () => {
         const bar = useCreateViewModel(BarVM);
 
         return `${foo.foo}${bar.bar} - ${bar.fooData}`;
+      };
+
+      const App = () => {
+        return (
+          <div>
+            <A />
+          </div>
+        );
+      };
+
+      const { container } = await act(async () =>
+        render(<App />, { wrapper: createVMStoreWrapper(vmStore) }),
+      );
+
+      await expect(container.firstChild).toMatchFileSnapshot(
+        `../../tests/snapshots/hooks/use-create-view-model/scenarios/${task.name}.html`,
+      );
+    });
+    test('accessing to the parent VM', async ({ task }) => {
+      const vmStore = new ViewModelStoreBaseMock();
+
+      class FooVM extends ViewModelBaseMock {
+        foo = 'foo';
+      }
+      class BarVM extends ViewModelBaseMock {
+        bar = 'bar';
+
+        get fooData() {
+          return this.viewModels.get(FooVM)?.foo + this.bar;
+        }
+      }
+
+      const B = () => {
+        const foo = useViewModel(FooVM);
+        const bar = useCreateViewModel(BarVM);
+
+        return `${foo.foo}, ${bar.bar} - ${bar.fooData}`;
+      };
+
+      const A = () => {
+        const foo = useCreateViewModel(FooVM);
+
+        return (
+          <>
+            {`A component with foo: "${foo.foo}"`}
+            <B />
+          </>
+        );
       };
 
       const App = () => {
