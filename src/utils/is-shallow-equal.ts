cdcb2c23c10b6de9e6513c23cbfe6b687b40fed8
Sergey Volkov <js2me@outlook.com>
2025-02-17 09:14:49 +0300

    fix: isShallowEqual incorrect comparison

--- src/utils/is-shallow-equal.ts
+++ src/utils/is-shallow-equal.ts
@@ -1,16 +1,23 @@
 import { isEqual } from 'lodash-es';
 
-import { AnyObject } from './types.js';
+import { AnyObject, Maybe } from './types.js';
 
-export const isShallowEqual = (a: AnyObject, b: AnyObject) => {
+export const isShallowEqual = (a: Maybe<AnyObject>, b: Maybe<AnyObject>) => {
+  if (a === b) {
+    return true;
+  }
+
+  if (
+    typeof a !== 'object' ||
+    typeof b !== 'object' ||
+    a === null ||
+    b === null
+  ) {
+    return false;
+  }
+
   const aKeys = Object.keys(a);
   const bKeys = Object.keys(b);
 
-  if (!isEqual(aKeys, bKeys)) return false;
-
-  for (const key of aKeys) {
-    if (a[key] !== b[key]) return false;
-  }
-
-  return true;
+  return isEqual(aKeys, bKeys) && aKeys.every((key) => a[key] === b[key]);
 };
