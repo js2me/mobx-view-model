efc6ebe6cdaef0107291ed5b4dbdc03410e22286
Sergey Volkov <js2me@outlook.com>
2025-07-05 13:38:44 +0300

    ci: fix gh releases

--- post-build.mjs
+++ post-build.mjs
@@ -1,4 +1,5 @@
-import { postBuildScript, publishScript } from 'js2me-exports-post-build-script';
+import { postBuildScript, publishScript, getInfoFromChangelog, publishGhRelease } from 'js2me-exports-post-build-script';
+import path from 'path';
 
 postBuildScript({
   buildDir: 'dist',
@@ -6,22 +7,56 @@ postBuildScript({
   srcDirName: 'src',
   filesToCopy: ['LICENSE', 'README.md', 'assets'],
   updateVersion: process.env.PUBLISH_VERSION,
-  onDone: (versionsDiff, { $ }, packageJson, { targetPackageJson}) => {
+  onDone: (versionsDiff, targetPackageJson, { $ }) => {
     if (process.env.PUBLISH) {
-      $(`pnpm test`);
+      if (!process.env.CI) {
+        $(`pnpm test`);
+        $('pnpm changeset version');
+      }
+
       // remove all test compiled files. TODO: find a better to ignore test files
       $('rm dist/**/*.test.*');
 
-      publishScript({
-        nextVersion: versionsDiff?.next ?? packageJson.version,
-        currVersion: versionsDiff?.current,
-        publishCommand: 'pnpm publish',
+      const nextVersion = versionsDiff?.next ?? targetPackageJson.data.version;
+
+      const publishOutput = publishScript({
+        gitTagFormat: '<tag>',
+        nextVersion: nextVersion,
+        packageManager: 'pnpm',
         commitAllCurrentChanges: true,
         createTag: true,
-        githubRepoLink: packageJson.repository.url.replace('git://', 'https://'),
-        cleanupCommand: 'pnpm clean', 
-        targetPackageJson,
-      })
+        safe: true,
+        onAlreadyPublishedThisVersion: () => {
+          console.warn(`${nextVersion} already published`);
+        },
+        cleanupCommand: 'pnpm clean',
+        targetPackageJson
+      });
+
+      if (process.env.CI) {
+        if (publishOutput.publishedGitTag) {
+          const { whatChangesText } = getInfoFromChangelog(
+            nextVersion,
+            path.resolve(targetPackageJson.locationDir, '../CHANGELOG.md'),
+            targetPackageJson.repositoryUrl
+          );
+
+          publishGhRelease({
+            authToken: process.env.GITHUB_TOKEN,
+            body: whatChangesText,
+            owner: targetPackageJson.ghRepoData.user,
+            repo: targetPackageJson.ghRepoData.packageName,
+            version: nextVersion,
+          })
+            .then((r) =>{
+              console.info('published new gh release',r)
+            })
+            .catch((err) =>{
+              console.error('failed to publish new gh release', err);
+              process.exit(1);
+            })
+        }
+      }
     }
   }
 });
