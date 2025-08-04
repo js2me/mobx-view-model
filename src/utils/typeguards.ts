18d4647e667311fd1581a1c3260d4b9e49ba4c9d
Sergey Volkov <js2me@outlook.com>
2025-08-04 09:56:39 +0300

    chore: fix unit tests (typeguards)

--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -5,8 +5,8 @@ import { AnyObject, Class, EmptyObject } from './types.js';
 
 export const isViewModel = <TPayload extends AnyObject = EmptyObject>(
   value: AnyObject,
-): value is ViewModel<TPayload> => 'payloadChanged' in value;
+): value is ViewModel<TPayload> => value.payloadChanged;
 
 export const isViewModelClass = <TPayload extends AnyObject = EmptyObject>(
   value: Function,
-): value is Class<ViewModel<TPayload>> => 'payloadChanged' in value.constructor;
+): value is Class<ViewModel<TPayload>> => value.prototype.payloadChanged;
