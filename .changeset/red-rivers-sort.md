3ab3d677a6b2a57b9b7c5a91e4502a4de45f1a19
Sergey Volkov <js2me@outlook.com>
2025-11-02 01:18:14 +0300

    fix: problems with bundled react and jsx-runtime

---
+++ .changeset/red-rivers-sort.md
@@ -0,0 +1,5 @@
+---
+"mobx-view-model": patch
+---
+
+fixed problems with using `React` and bundled jsx-runtime
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,4 @@
 import { defineLibViteConfig } from "sborshik/vite";
 import { ConfigsManager } from "sborshik/utils";
  
-export default defineLibViteConfig(ConfigsManager.create(), {
-    build: {
-        rollupOptions: {
-            external: [
-                'react/jsx-runtime',
-              ],
-        },
-    }
-})
+export default defineLibViteConfig(ConfigsManager.create())
