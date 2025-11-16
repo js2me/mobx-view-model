b3c6b53b0e76ab95d21761e5d8f81517c01fb057
Sergey Volkov <js2me@outlook.com>
2025-11-16 09:32:25 +0300

    feat: improve typeguards

---
+++ .changeset/itchy-masks-know.md
@@ -0,0 +1,5 @@
+---
+"mobx-view-model": minor
+---
+
+improved typeguards (isViewModelClass, isViewModelSimpleClass, isViewModel) by pass parent view model generic type
--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -1,10 +1,30 @@
 import type { AnyObject, Class, EmptyObject } from 'yummies/types';
-import type { ViewModel } from '../view-model/index.js';
+import type {
+  AnyViewModel,
+  AnyViewModelSimple,
+  ViewModel,
+  ViewModelSimple,
+} from '../view-model/index.js';
 
-export const isViewModel = <TPayload extends AnyObject = EmptyObject>(
+export const isViewModel = <
+  TPayload extends AnyObject = EmptyObject,
+  ParentViewModel extends AnyViewModel | AnyViewModelSimple | null = null,
+>(
   value: AnyObject,
-): value is ViewModel<TPayload> => value.payloadChanged;
+): value is ViewModel<TPayload, ParentViewModel> => value.payloadChanged;
 
-export const isViewModelClass = <TPayload extends AnyObject = EmptyObject>(
+export const isViewModelClass = <
+  TPayload extends AnyObject = EmptyObject,
+  ParentViewModel extends AnyViewModel | AnyViewModelSimple | null = null,
+>(
   value: Function,
-): value is Class<ViewModel<TPayload>> => value.prototype.payloadChanged;
+): value is Class<ViewModel<TPayload, ParentViewModel>> =>
+  value.prototype.payloadChanged;
+
+export const isViewModeSimpleClass = <
+  TPayload extends AnyObject = EmptyObject,
+  ParentViewModel extends AnyViewModel | AnyViewModelSimple | null = null,
+>(
+  value: Function,
+): value is Class<ViewModelSimple<TPayload, ParentViewModel>> =>
+  !isViewModelClass(value);
